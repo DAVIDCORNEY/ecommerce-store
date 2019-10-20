b56abcfc97dc2d0cb879ab143bf29687f575b799
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-20 18:04:59 +0100

    homepage component add 5 menu items

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -5,7 +5,31 @@ const HomePage = () => (
     <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">SNEAKERS</h1>
+          <h1 className="title">TRAINERS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">BOOTS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SHOES</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WOMEN</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MEN</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>

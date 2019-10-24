997e224187521ae0a2a2fdd25630dad9529bed8b
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-10-24 20:29:50 +0100

    MenuItem change order of  dynamic class size

--- src/components/MenuItem/MenuItem.component.jsx
+++ src/components/MenuItem/MenuItem.component.jsx
@@ -7,10 +7,10 @@ const MenuItem = ({ title, imageUrl, size }) => (
     style={{
       backgroundImage: `url(${imageUrl})`
     }}
-    className={`${size} menu-item`}
+    className={`menu-item ${size}`}
   >
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>

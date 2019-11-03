cf12ec44b437e984fb675738c3558d5ec97142c0
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-11-03 14:04:21 +0000

    Import redux persist and add config to persist data

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; //here we get local storage from window browser
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root", //the point at which we start storing everything
+  storage,
+  whitelist: ["cart"] //the reducer we want to persist. User is handled by firebase.
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);

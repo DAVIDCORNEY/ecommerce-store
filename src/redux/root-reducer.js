64b0c158404b408e37e1e50092d9c048a581983f
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-11-03 14:51:29 +0000

    Move Directory component state into redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import storage from "redux-persist/lib/storage"; //here we get local storage fro
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
 
 const persistConfig = {
   key: "root", //the point at which we start storing everything
@@ -13,7 +14,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);

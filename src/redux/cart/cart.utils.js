6426f77a32a10bad8baccf94639e89950a74c313
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-11-04 14:12:36 +0000

    Fix bug in CheckoutItem to remove an item if quantity is below 1

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -16,8 +16,8 @@ export const removeSingleItem = (cartItems, itemToRemove) => {
   const existingCartItem = cartItems.find(
     cartItem => cartItem.id === itemToRemove.id
   );
-  if (existingCartItem === 1) {
-    cartItems.filter(cartItem => cartItem.id !== itemToRemove.id);
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== itemToRemove.id);
   }
 
   return cartItems.map(cartItem =>

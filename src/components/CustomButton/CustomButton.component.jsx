0631dc6dc7cf838a5a586b22b63b1a66dd125f7a
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-11-12 14:54:18 +0000

    Remove redundant code from CustomButton component and remove uneccessary scss files

--- src/components/CustomButton/CustomButton.component.jsx
+++ src/components/CustomButton/CustomButton.component.jsx
@@ -1,24 +1,6 @@
 import React from "react";
 
-// import "./CustomButton.style.scss";
-
 import { CustomButtonContainer } from "./CustomButton.styles";
-
-// const CustomButton = ({
-//   children,
-//   isGoogleSignIn,
-//   invertedColor,
-//   ...otherProps
-// }) => (
-//   <button
-//     className={`${invertedColor ? "inverted-color" : ""}${
-//       isGoogleSignIn ? "google-sign-in" : ""
-//     } custom-button`}
-//     {...otherProps}
-//   >
-//     {children}
-//   </button>
-// );
 
 const CustomButton = ({ children, ...props }) => (
   <CustomButtonContainer {...props}>{children}</CustomButtonContainer>

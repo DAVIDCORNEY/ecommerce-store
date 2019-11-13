e0041f0bdd898b38205a37732d4327028df3fe36
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-11-13 18:11:49 +0000

    Change CollectionItem component use styled components

--- src/components/CollectionItem/CollectionItem.component.jsx
+++ src/components/CollectionItem/CollectionItem.component.jsx
@@ -1,34 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import CustomButton from "../CustomButton/CustomButton.component";
 import { addItem } from "../../redux/cart/cart.actions";
 
-import "./CollectionItem.styles.scss";
+import {
+  CollectionItemContainer,
+  AddButton,
+  BackgroundImage,
+  CollectionFooterContainer,
+  NameContainer,
+  PriceContainer
+} from "./CollectionItem.styles";
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
   return (
-    <div className="collection-item">
-      <div
-        className="image"
-        style={{
-          backgroundImage: `url(${imageUrl})`
-        }}
-      />
-      <div className="collection-footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
-      </div>
-      <CustomButton
-        className="custom-button"
-        onClick={() => addItem(item)}
-        invertedColor
-      >
+    <CollectionItemContainer>
+      <BackgroundImage className="image" imageUrl={imageUrl} />
+      <CollectionFooterContainer>
+        <NameContainer>{name}</NameContainer>
+        <PriceContainer>{price}</PriceContainer>
+      </CollectionFooterContainer>
+      <AddButton onClick={() => addItem(item)} invertedColor>
         {" "}
         Add to basket{" "}
-      </CustomButton>
-    </div>
+      </AddButton>
+    </CollectionItemContainer>
   );
 };
 

254b10dcc77c4f59c24c0514999789026fa7046b
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-11-20 20:42:53 +0000

    Change SignIn Component to use styled components

--- src/components/SignIn/SignIn.component.jsx
+++ src/components/SignIn/SignIn.component.jsx
@@ -5,7 +5,11 @@ import CustomButton from "../CustomButton/CustomButton.component";
 
 import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
-import "./SignIn.styles.scss";
+import {
+  SignInContainer,
+  SignInTitle,
+  SignInButtons
+} from "./SignIn.styles.jsx";
 
 class SignIn extends Component {
   constructor(props) {
@@ -37,8 +41,8 @@ class SignIn extends Component {
 
   render() {
     return (
-      <div className="sign-in">
-        <h2>I already have an account</h2>
+      <SignInContainer>
+        <SignInTitle>I already have an account</SignInTitle>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
@@ -58,14 +62,14 @@ class SignIn extends Component {
             label="password"
             required
           />
-          <div className="buttons">
+          <SignInButtons>
             <CustomButton type="submit">Sign In</CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
               Sign In With Google
             </CustomButton>
-          </div>
+          </SignInButtons>
         </form>
-      </div>
+      </SignInContainer>
     );
   }
 }

f8fe24e7cdeababe96485cff643c6d96ade38943
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-11-22 15:57:05 +0000

    Change Checkout Page to use styled components

--- src/pages/Checkout/Checkout.component.jsx
+++ src/pages/Checkout/Checkout.component.jsx
@@ -10,41 +10,47 @@ import {
   selectCartTotal
 } from "../../redux/cart/cart.selectors";
 
-import "./Checkout.styles.scss";
+import {
+  CheckoutPageContainer,
+  CheckoutHeaderContainer,
+  HeaderBlockContainer,
+  TotalContainer,
+  StripeTestWarningContainer
+} from "./Checkout.styles.jsx";
 
 const Checkout = ({ cartItems, total }) => (
-  <div className="checkout-page">
-    <div className="checkout-header">
-      <div className="header-block">
+  <CheckoutPageContainer>
+    <CheckoutHeaderContainer>
+      <HeaderBlockContainer>
         <span>Product</span>
-      </div>
-      <div className="header-block">
+      </HeaderBlockContainer>
+      <HeaderBlockContainer>
         <span>Description</span>
-      </div>
-      <div className="header-block">
+      </HeaderBlockContainer>
+      <HeaderBlockContainer>
         <span>Quantity</span>
-      </div>
-      <div className="header-block">
+      </HeaderBlockContainer>
+      <HeaderBlockContainer>
         <span>Price</span>
-      </div>
-      <div className="header-block">
+      </HeaderBlockContainer>
+      <HeaderBlockContainer>
         <span>Remove</span>
-      </div>
-    </div>
+      </HeaderBlockContainer>
+    </CheckoutHeaderContainer>
     {cartItems.map(cartItem => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <div className="total">
+    <TotalContainer>
       <span>TOTAL: £{total}</span>
-    </div>
-    <div className="stripe-test-warning">
+    </TotalContainer>
+    <StripeTestWarningContainer>
       *Please use the following test credit card details to simulate a card
       payment*
       <br />
       4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
-    </div>
+    </StripeTestWarningContainer>
     <StripeButton price={total} />
-  </div>
+  </CheckoutPageContainer>
 );
 
 const mapStateToProps = createStructuredSelector({

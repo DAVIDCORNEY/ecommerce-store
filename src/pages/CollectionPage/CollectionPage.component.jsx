183bc78c41c922d8189793ab83cc263ebc3398a3
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-11-22 17:06:06 +0000

    Change Collection Page to use styled components

--- src/pages/CollectionPage/CollectionPage.component.jsx
+++ src/pages/CollectionPage/CollectionPage.component.jsx
@@ -5,19 +5,23 @@ import CollectionItem from "../../components/CollectionItem/CollectionItem.compo
 
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
-import "./CollectionPage.styles.scss";
+import {
+  CollectionPageContainer,
+  CollectionTitle,
+  CollectionItems
+} from "./CollectionPage.styles.jsx";
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
+    <CollectionPageContainer>
+      <CollectionTitle>{title}</CollectionTitle>
+      <CollectionItems>
         {items.map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
-    </div>
+      </CollectionItems>
+    </CollectionPageContainer>
   );
 };
 const mapStateToProps = (state, ownProps) => ({
